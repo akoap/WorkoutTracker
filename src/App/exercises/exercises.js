738d8314625946e12039eaea5f88fc881559cd11
Akshay Patil <avpat02@gmail.com>
2020-12-24 14:17:49 -0500

    added logout

--- src/App/exercises/exercises.js
+++ src/App/exercises/exercises.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './exercises.css';
+import { useAuth } from './../../contexts/AuthContext';
+import { Redirect } from 'react-router-dom';
 
 export default function Exercises() {
-    return (
-        <>
-            <div className="Box">
-                <h3>Hello</h3>
-            </div>
-            <div className="Box">
-                <p></p>
-            </div>
-        </>
-    );
+    const { currentUser } = useAuth();
+    if (currentUser) {
+        return (
+            <>
+                <div className="Box">
+                    <h3>Hello</h3>
+                </div>
+                <div className="Box">
+                    <p></p>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <Redirect to='/login' />
+        );
+    }
+    
 }
